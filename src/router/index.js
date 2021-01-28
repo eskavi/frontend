702b449e17e69924ab29dcd697ae43e3e6979391
Niv Adam <nivadam@web.de>
2021-01-28 17:33:08 +0100

    Update router configuration with new routes

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,28 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/modules',
+    name: 'Modules',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "modules" */ '../views/ModulesOverview.vue'),
+  },
+  {
+    path: '/configurator',
+    name: 'Configurator',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "configurator" */ '../views/Configurator.vue'),
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "configurator" */ '../views/Account.vue'),
   },
 ];
 
