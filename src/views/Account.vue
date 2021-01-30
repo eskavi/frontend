f8bf0c334ce9e8fd87de9c35782ea6ce184caa79
Niv Adam <nivadam@web.de>
2021-01-30 14:33:22 +0100

    Incorporate AccountSettingsComponent

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="account">
-    <p>Account page</p>
+    <AccountSettings />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import AccountSettings from '../components/AccountSettings.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    AccountSettings,
+  },
+};
+</script>
