e1093e0c953e5cd7285c3b0b716e009c40255f9b
Niv Adam <nivadam@web.de>
2021-01-30 14:32:46 +0100

    Add AccountSettings component

---
+++ src/components/AccountSettings.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row justify="center" class="ma-5">
+      <v-card width="50%">
+        <v-card-title>Account Settings</v-card-title>
+
+        <div class="mx-auto text-center">
+          <v-avatar color="brown" size="70">
+            <span class="white--text headline">MM</span>
+          </v-avatar>
+          <h3 class="ma-2">Manuel Müllerschön</h3>
+          <v-chip>Opa</v-chip>
+          <v-chip>Administrator</v-chip>
+        </div>
+        <div class="mx-auto text-center ma-10">
+          <v-btn>Change Password</v-btn><br />
+          <v-btn class="ma-3">Logout</v-btn><br />
+          <v-btn class="error">Delete Account</v-btn>
+        </div>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
