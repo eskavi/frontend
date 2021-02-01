6c7903e4b1ea63db6514e38bdc161046bb824a8d
Niv Adam <nivadam@web.de>
2021-02-01 15:21:49 +0100

    Add View and Component for an implementation creator stepper

---
+++ src/views/CreateModuleImp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="configurator">
+    <v-container>
+      <ModuleImpStepper />
+    </v-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ModuleImpStepper from '../components/ModuleImpStepper.vue';
+
+export default {
+  name: 'CreateModuleImp',
+  components: {
+    ModuleImpStepper,
+  },
+};
+</script>
