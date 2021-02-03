2580ee21c77812ce45077c465026e97b0f21b6e3
Niv Adam <nivadam@web.de>
2021-02-03 20:31:43 +0100

    Only allow logged in users to access account page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import store from '../store/index';
 
 Vue.use(VueRouter);
 
@@ -33,6 +34,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "configurator" */ '../views/Account.vue'),
+    beforeEnter: (to, from, next) => {
+      if (!store.state.user.email || !store.state.user.token) {
+        store.dispatch('sendActionResponse', 'You need to be logged in to view this page.');
+        next('/');
+      } else {
+        next();
+      }
+    },
   },
   {
     path: '/impcreator',
