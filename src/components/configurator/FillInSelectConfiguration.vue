d46c7bdcff5bda852af9c2e51b4e7cd794d9e593
Niv Adam <nivadam@web.de>
2021-02-17 17:28:04 +0100

    Add Select field

---
+++ src/components/configurator/FillInSelectConfiguration.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-row justify="center">
+    <v-card width="90%" class="mx-2 pa-3">
+      <v-row justify="center">
+        <v-card-title class="ma-2">Select Field</v-card-title>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="12" md="6">
+          <v-text-field label="Name of Select Field" v-model="selectField.name"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-combobox
+            v-model="selectField.content"
+            label="Enter all selection possibilities here"
+            deletable-chips
+            multiple
+            chips
+          >
+          </v-combobox>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+            label="Start of key expression"
+            v-model="selectField.keyExpression.expresssionStart"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+            label="End of key expression"
+            v-model="selectField.keyExpression.expresssionEnd"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'FillInSelectConfiguration',
+  props: {
+    selectField: Object,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onBuildUp() {},
+  },
+  mounted() {
+    this.onBuildUp();
+  },
+};
+</script>
